b427d121a66b61b9e6c034b3ca2b836af3fd721b
Nardis <nardis9501@gmail.com>
2023-10-12 19:34:38 -0400

    fix: remove text selection when changing theme

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Layout({ children }) {
   return (
     <>
-      <div className="flex items-center  h-screen w-[1200px] p-2">
+      <div className="flex select-none items-center  h-screen w-[1200px] p-2">
         {children}
       </div>
     </>
